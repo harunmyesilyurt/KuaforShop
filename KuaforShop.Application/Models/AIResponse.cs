2786fcedb782a0c13505f1fc18a333544cdb8514
harunmyesilyurt <harunmahmutyesilyurt@gmail.com>
2024-12-28 01:52:44 +0300

    [update] project

---
+++ KuaforShop.Application/Models/AIResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuaforShop.Application.Models
+{
+    public class AIResponse
+    {
+        public bool Success { get; set; }
+        public Data Data { get; set; }
+    }
+    public class Shape
+    {
+        public string ClassName { get; set; }
+        public string Probability { get; set; }
+    }
+
+    public class Data
+    {
+        public List<Shape> Shapes { get; set; }
+        public List<string> Suggestions { get; set; }
+    } 
+}
---
+++ KuaforShop.Application/Utils/AIHelper.cs
@@ -0,0 +1,46 @@
+﻿using KuaforShop.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuaforShop.Application.Utils
+{
+    public class AIHelper
+    {
+        public async Task<AIResponse> CreateHair(string path) 
+        {
+            string apiUrl = "http://node-js-faceshape.onrender.com/api/ai";
+            string imagePath = path;
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (var formData = new MultipartFormDataContent())
+                {
+                    byte[] imageData = await File.ReadAllBytesAsync(imagePath);
+                    var imageContent = new ByteArrayContent(imageData);
+                    imageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/jpeg");
+
+                    formData.Add(imageContent, "image", Path.GetFileName(imagePath));
+
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, formData);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Response from API:");
+                        Console.WriteLine(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode}");
+                        string errorResponse = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(errorResponse);
+                    }
+                }
+            }
+
+            return new AIResponse();
+        }
+    }
+}
